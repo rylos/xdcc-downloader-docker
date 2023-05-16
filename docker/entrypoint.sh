6ec4c8a015abf1df310abead6304607508478f3e
rylos <rylos78@gmail.com>
2023-05-16 14:43:01 +0200

    bug

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-exec /usr/bin/python /opt/playxz/XdccDownloaderWeb/XdccDownloaderServer.pyc start
-#/etc/init.d/XdccDownloaderWeb restart
-#tail -f /dev/null
+#exec /usr/bin/python /opt/playxz/XdccDownloaderWeb/XdccDownloaderServer.pyc start
+exec /etc/init.d/XdccDownloaderWeb restart
+tail -f /dev/null
